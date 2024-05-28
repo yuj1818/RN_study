14c6cfac778d20119c162e1481bbde32d410f628
Yu Jeong SON <po042@naver.com>
2024-05-28 09:10:20 +0900

    remove .git file

---
+++ day2_flexbox/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ 
+      padding: 50, 
+      flexDirection: 'row', 
+      width: '80%', 
+      height: 300,
+      justifyContent: 'space-around',
+      alignItems: 'stretch'
+    }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'blue',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 2
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
+    </View>
+  );
+}
